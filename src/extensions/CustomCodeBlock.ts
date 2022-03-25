97a608f2cde1ef1fade25401184186514e9982d3
ohah <bookyoon173@gmail.com>
2022-03-25 15:38:25 +0900

    update

--- src/extensions/CustomCodeBlock.ts
+++ src/extensions/CustomCodeBlock.ts
@@ -1,7 +1,10 @@
 import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
-import { Node, NodeView, NodeViewProps, NodeViewRendererProps } from "@tiptap/core"
+import { Node, NodeView, NodeViewProps, NodeViewRendererProps, Editor, NodeViewRendererOptions, getSchema } from "@tiptap/core"
+import { Decoration, NodeView as ProseMirrorNodeView} from 'prosemirror-view'
 import lowlight from "lowlight";
 import mermaid from "mermaid";
+class PureNodeView extends NodeView<any, Editor, NodeViewRendererOptions> {
+}
 export enum MODE {
   PREVIEW,
   EDIT
@@ -18,30 +21,92 @@ export const CustomBlockExtension = CodeBlockLowlight
           decorations : decorations,
           extension : extension
         }
-        const CustomNode = new NodeView("<div> asdf </div>", props);
-        CustomNode.mount();
+        // const CustomNode = new NodeView("<div>무야호</div>", props);
         if(node.attrs.language === "mermaid") {
+          // const dom = document.createElement('div');
+          // const wrapper = document.createElement('div');
+          // console.log(extension);
+          // // dom.contentEditable = "true";
+          // dom.textContent = /\<\"((.|\n)*)\"\>/.exec(node.content.toString())[1].replace(/\\n/g, '\n')
+          // dom.className = "mermaid";
+          // mermaid.initialize({
+          //   startOnLoad:true,
+          //   htmlLabels:true,
+          // });
+          // // dom.dataset.dataNodeViewContent = "";
+          // // wrapper.dataset.dataNodeViewWrapper = "";
+          // // console.log('element', CustomNode.node);
+          // // wrapper.append(CustomNode.dom);
+          // wrapper.appendChild(dom);
           const dom = document.createElement('div');
-          const wrapper = document.createElement('div');
-          console.log(editor, node, extension, CustomNode);
-          // dom.contentEditable = "true";
-          console.log(node.content.toString().replace(/\\t/g, ''))
-          dom.textContent = /\<\"((.|\n)*)\"\>/.exec(node.content.toString())[1].replace(/\\n/g, '\n')
-          dom.className = "mermaid";
-          mermaid.initialize({startOnLoad:true});
-          dom.dataset.dataNodeViewContent = "";
-          wrapper.dataset.dataNodeViewWrapper="";
-          console.log('element', CustomNode.node);
-          wrapper.append(CustomNode.dom);
+          dom.classList.add('mermaid-editor-btn-group');
+
+          const source = document.createElement('button');
+
+          source.classList.add('source');
+          source.innerHTML = `<i class="ri-code-s-slash-line"></i>`;
+          source.contentEditable = "false";
+
+          const preview = document.createElement('button');
+
+          preview.classList.add('preview');
+          preview.innerHTML = `<i class="ri-organization-chart"></i>`;
+          preview.contentEditable = "false";
+
+          const pre = document.createElement('pre');
+          pre.style.marginTop = "0"
+          const code = document.createElement('code');
+          code.classList.add(`language-${node.attrs.language}`)
+          
+          console.log(node.attrs);
+          if(node.attrs.source === "hidden") {
+            pre.classList.add("hidden");
+          }
+          pre.setAttribute("language", "mermaid");
+          code.classList.add('mermaid');
+          pre.append(code)
+
+          const content = document.createElement('code');
+          content.setAttribute("language", "mermaid");
+          content.classList.add('content');
+          content.classList.add(`language-${node.attrs.language}`)
+          // content.innerHTML = /\<\"((.|\n)*)\"\>/.exec(node.content.toString())[1].replace(/\\n/g, '\n')
+          content.textContent = /\<\"((.|\n)*)\"\>/.exec(node.content.toString())[1].replace(/\\n/g, '\n')
+          content.classList.add("mermaid");
+          (window as any).mermaid = mermaid;
+          if(node.attrs.content === "hidden") {
+            content.classList.add("hidden");
+          }else {
+          }
+          dom.append(source, preview, pre, content);
+          source.addEventListener('click', (e)=>{
+            if(node.attrs.source === "hidden") {
+              node.attrs.source = "";
+              pre.classList.remove('hidden');
+            } else {
+              node.attrs.source = "hidden";
+              pre.classList.add('hidden');
+            }
+          });
+          
+          
+          preview.addEventListener('click', (e)=>{
+            node.attrs.content = "";
+            content.classList.remove('hidden');
+          })
+          mermaid.initialize({
+            startOnLoad:true,
+            htmlLabels:true,
+          });
           return {
-            dom : wrapper,
-            contentDOM : dom,
+            dom : dom,
+            // contentDOM : code,
           }
         } else {
-          return {
-            dom : CustomNode.mount(),
-            contentDOM : CustomNode
-          }
+          // return {
+          //   dom : CustomNode.mount(),
+          //   contentDOM : CustomNode
+          // }
         }
       }
     },
@@ -52,6 +117,18 @@ export const CustomBlockExtension = CodeBlockLowlight
         },
         language: {
           default: null
+        },
+        class : {
+          default : "",
+        },
+        source : {
+          default : ""
+        },
+        preview : {
+          default : ""
+        },
+        content : {
+          default : "",
         },
         id: {
           // default: 'content',
--- src/index.ts
+++ src/index.ts
@@ -673,20 +673,26 @@ class Editor {
 (function () {
   new Editor({
     element: document.querySelector("#container"),
-    content: `<pre><code class="language-mermaid">
-flowchart TD
+    content: `<pre><code class="language-mermaid">flowchart TD
 A[Start] --> B{Is it?};
 B -- Yes --> C[OK];
 C --> D[Rethink];
 D --> B;
 B -- No ----> E[End];</code></pre>
 <pre><code class="language-javascript">console.log('asdf');</code></pre>
+<pre><code class="language-mermaid">graph TD
+A[Christmas] -->|Get money| B(Go shopping)
+B --> C{Let me think}
+C -->|One| D[Laptop]
+C -->|Two| E[iPhonee]
+C -->|Three| F[Cars]</code></pre>
+<pre><code class="language-javascript">console.log('asdf');</code></pre>
 <img src="https://i.ytimg.com/vi/-6Zjub7CH4k/hqdefault.jpg" />
     `,
     editorProps : {
       attributes : {
         class:"editor-body",
-        style : "height:300px;overflow-y:auto"
+        style : "height:800px;overflow-y:auto"
       }
     },
     ImageUpload : function(files) {
